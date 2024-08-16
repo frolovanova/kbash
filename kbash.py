70a6f80053bd67cfc584f27cdace14d9531a52a1
Alexander Fournier <afournier@stagingterritory.com>
2024-08-15 22:01:13 -0400

    spawns root session in home directory

--- kbash.py
+++ kbash.py
@@ -43,14 +43,17 @@ def rename_statefulset(namespace, old_statefulset_name, new_statefulset_name):
         print(f"Failed to delete old StatefulSet '{old_statefulset_name}': {e}")
 
 def kexec_and_run_bash(namespace, pod_name, container_name):
+    # Derive the username from the pod name
+    user_dir = pod_name.split('-')[0]  # Assume the pod name is in the format "<username>-<statefulset-name>-<index>"
+    
     try:
-        # Construct the kubectl exec command
+        # Construct the kubectl exec command with directory creation if it doesn't exist
         command = [
             "kubectl", "exec", "-it",
             pod_name,
             "-n", namespace,
             "-c", container_name,
-            "--", "/bin/bash"
+            "--", "bash", "-c", f"mkdir -p /home/user/{user_dir} && cd /home/user/{user_dir} && exec bash"
         ]
         
         # Execute the command
@@ -60,7 +63,7 @@ def kexec_and_run_bash(namespace, pod_name, container_name):
         sys.exit(1)
 
 def main():
-    parser = argparse.ArgumentParser(description="Rename a StatefulSet or exec into a Pod's bash.")
+    parser = argparse.ArgumentParser(description="Rename a StatefulSet or exec into a Pod's bash restricted to a user's directory.")
     parser.add_argument("--rename", nargs=2, metavar=('OLD_STATEFULSET_NAME', 'NEW_STATEFULSET_NAME'), help="Rename the StatefulSet from OLD_STATEFULSET_NAME to NEW_STATEFULSET_NAME.")
     parser.add_argument("--namespace", default="lab", help="The namespace where the StatefulSet is located (default: 'lab').")
     parser.add_argument("--pod_name", help="The name of the Pod to exec into.")
@@ -72,7 +75,7 @@ def main():
         old_statefulset_name, new_statefulset_name = args.rename
         rename_statefulset(args.namespace, old_statefulset_name, new_statefulset_name)
     elif args.pod_name:
-        # Exec into the pod and start a bash shell
+        # Exec into the pod and start a bash shell restricted to the user's directory
         kexec_and_run_bash(args.namespace, args.pod_name, args.container_name)
     else:
         print("Error: You must provide either --rename to rename a StatefulSet or --pod_name to exec into a Pod's bash.")
